44c3190263fb457a94d8709bcb00872d8b857eb1
Charlie Parrott <cparrott5700@gmail.com>
2021-09-13 15:27:33 -0400

    Adding personal portfolio files

---
+++ src/components/pages/About.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <div class="container-aboutme">
+      <div class="row align-items-center">
+        <div id="about-me" class="about-title col-5">About Me</div>
+        <div class="about-section col-7">
+          <h1>About Page</h1>
+          <h6>Web Development</h6>
+          <p>I am Charles Parrott and I am currently learning how to be a great web developer,
+            learning HTML, Css, and Javascript. This is my about me and I will be updating it as the class continues.
+            I look forward to the future work!
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/pages/Projects.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+export default function Projects() {
+  return (
+    <div class="container-projects">
+      <div class="row align-items-center">
+        <div class="col-8">
+  
+          <row>
+            <div class="card-cov">
+              <img src="./assets/images/Cov-Track.png" class="card-img-top" alt="..." />
+              <div class="card-body">
+                <a href="#https://jamesljenks.github.io/07_Project-1_CovTrack/" class="btn btn-primary">CovTrack</a>
+              </div>
+            </div>
+        </row>
+  
+        <row class="row projects-lower">
+          <div class="card-work col-sm-5">
+            <div class="card-body">
+              <img src="./assets/images/Work-Scheduler.PNG" class="card-img-top" alt="..." />
+              <a href="#https://crparrott.github.io/Work-Schedule-Homework/" class="btn btn-primary">Work Scheduler</a>
+            </div>
+          </div>
+  
+          <div class="card-code col-sm-5">
+            <div class="card-body">
+              <img src="./assets/images/ToDieinAustralia.png" class="card-img-top" alt="..." />
+              <a href="#https://https://github.com/CRParrott/todieinaustralia" class="btn btn-primary">ToDieinAustralia</a>
+            </div>
+          </div>
+        </row>
+  
+        </div>
+        <div id="projects" class="projects">Projects</div>
+      </div>
+    </div>
+  );
+}
